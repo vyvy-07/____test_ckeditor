47face631213400b45542d5069e7e4d171a9fa58
vyvy-07 <nguyenthuyvy12all@gmail.com>
2024-12-11 14:53:49 +0700

    .

--- src/app/tiptap/page.tsx
+++ src/app/tiptap/page.tsx
@@ -1,17 +1,11 @@
 'use client';
 
-import TipTapComponent from '@/components/TipTapEditor';
-import TipTapEditorMui from '@/components/TipTapEditorMui';
+import TipTapMUI from '@/components/TipTapEditorMui';
 
 const TipTapEditor = () => {
   return (
-    <div className="pt-7 grid grid-cols-2 gap-3 ">
-      <>
-        <TipTapComponent />
-      </>
-      <>
-        <TipTapEditorMui />
-      </>
+    <div className="p-7 grid grid-cols-2 gap-3 ">
+      <TipTapMUI />
     </div>
   );
 };
