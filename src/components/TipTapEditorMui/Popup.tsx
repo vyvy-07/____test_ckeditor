62379ff956f5a387f490b6791f52a1d199e9f07c
vyvy-07 <nguyenthuyvy12all@gmail.com>
2024-12-31 11:07:12 +0700

    ver-1

--- src/components/TipTapEditorMui/Popup.tsx
+++ src/components/TipTapEditorMui/Popup.tsx
@@ -4,14 +4,14 @@ const PopupMedia = ({ image }: { image: (src: string) => void }) => {
   const [sourceImage, setSourceImage] = useState('');
   console.log('sourceImage :>> ', sourceImage);
   return (
-    <>
+    <div className="fixed w-full h-full bg-white z-[90]">
       <p
         onClick={() => image(sourceImage)}
         className="block w-full text-[24px] top-0 text-end P-3 "
       >
         X
       </p>
-      <div className="grid grid-cols-4 grid-rows-auto fixed w-full h-full bg-white z-[90] gap-4">
+      <div className="grid grid-cols-4 grid-rows-auto  gap-4">
         {DATA_IMAGE?.map((item) => (
           <div
             key={item?.src}
@@ -26,7 +26,7 @@ const PopupMedia = ({ image }: { image: (src: string) => void }) => {
           </div>
         ))}
       </div>
-    </>
+    </div>
   );
 };
 
